07c99507b2d83cb261bb385303a854f92b465317
pushpendrajain <jainpushp1990@gmail.com>
2022-01-25 19:37:51 +0530

    Code changes

--- Design-Calculator-New/Controllers/HomeController.cs
+++ Design-Calculator-New/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Design_Calculator_New.Entity;
+using Design_Calculator_New.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,67 @@ namespace Design_Calculator_New.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleDBEntities Dbcontext = new SimpleDBEntities();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Login(UserModel userModel)
         {
-            ViewBag.Message = "Your contact page.";
+            var iSUserExists = Dbcontext.Employees.Any(x => x.UserName == userModel.UserName && x.Password == userModel.Password);
+            if (iSUserExists)
+            {
+                return RedirectToAction("Calculate");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Calculate()
+        {
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Calculate(GoldModel goldModel)
+        {
+            if (goldModel.Discount == 0)
+            {
+                int totalPrice = CalculateTotalPriceWithoutDiscount(goldModel.GoldPrice, goldModel.Weightitem);
+                TempData["GoldPrice"] = goldModel.GoldPrice;
+                TempData["Weightitem"] = goldModel.Weightitem;
+                TempData["Discount"] = goldModel.Discount;
+                TempData["totalPrice"] = totalPrice;
+                return RedirectToAction("Calculate");
+            }
+            else
+            {
+                int totalPrice = CalculateTotalPricewithDiscount(goldModel.GoldPrice, goldModel.Weightitem, goldModel.Discount);
+                TempData["GoldPrice"] = goldModel.GoldPrice;
+                TempData["Weightitem"] = goldModel.Weightitem;
+                TempData["Discount"] = goldModel.Discount;
+                TempData["totalPrice"] = totalPrice;
+                return RedirectToAction("Calculate");
+            }
+        }
+
+        public static int CalculateTotalPriceWithoutDiscount(int Goldprice, int WeightoftheGold)
+        {
+            int totalPricewithoutDiscount = Goldprice * WeightoftheGold;
+            return totalPricewithoutDiscount;
+        }
+
+        public static int CalculateTotalPricewithDiscount(int Goldprice, int WeightoftheGold, int discount)
+        {
+            int totalpr = Goldprice * WeightoftheGold;
+            int Totaldiscount = totalpr * discount / 100;
+            int totalPricewithDiscount = totalpr - Totaldiscount;
+            return totalPricewithDiscount;
         }
     }
 }
---
+++ Design-Calculator-New/Models/GoldModel .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Design_Calculator_New.Models
+{
+    public class GoldModel
+    {
+        public int GoldPrice { get; set; }
+
+        public int Weightitem { get; set; }
+
+        public int Discount { get; set; }
+
+        public int TotalPriceItem { get; set; }
+    }
+}
